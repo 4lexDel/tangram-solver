ebd5fd2e0292b4c6254c0f069c949108be0a7bfc
Alexandre DELORME <alexandre.delorme@viacesi.fr>
2024-02-18 01:21:03 +0100

    build v1

--- sandbox/lit/assets/js/script.js
+++
@@ -1,7 +0,0 @@
-import { HelloWorldComponent } from "../components/hello-world.component.js";
-import { ConfirmModalComponent } from "../components/confirm-modal.component.js";
-import { AppComponent } from "../components/app.component.js";
-
-// let testComponent = document.getElementById('test');
-
-// testComponent.message = "sdfbgf";
